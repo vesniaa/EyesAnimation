0621f95abd970db23d70a64ffb9a3b36f750d573
Eugenia <pyssiki@yandex.ru>
2022-05-31 19:01:37 +0500

    Initial Commit

---
+++ EyesAnimation/EyesAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  EyesAnimationApp.swift
+//  EyesAnimation
+//
+//  Created by Евгения Аникина on 31.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct EyesAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
